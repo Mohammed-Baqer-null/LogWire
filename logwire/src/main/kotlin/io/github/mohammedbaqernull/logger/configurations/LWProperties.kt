81ac15951f7032f8ec4fe308252ed3db2f75ee85
Tom <bakrfadl123.iq@gmail.com>
2025-11-26 14:20:16 +0300

    Update license information in LWProperties.kt

--- logwire/src/main/kotlin/io/github/mohammedbaqernull/logger/configurations/LWProperties.kt
+++ logwire/src/main/kotlin/io/github/mohammedbaqernull/logger/configurations/LWProperties.kt
@@ -1,18 +1,18 @@
 /*
- *  This file is part of AndroidCodeStudio.
+ *  This file is part of Logwire.
  *
- *  AndroidCodeStudio is free software: you can redistribute it and/or modify
+ *  Logwire is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  AndroidCodeStudio is distributed in the hope that it will be useful,
+ *  Logwire is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with AndroidCodeStudio.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with Logwire.  If not, see <https://www.gnu.org/licenses/>.
  */
   
 package io.github.mohammedbaqernull.logger.configurations
